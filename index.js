828a966027d4238cb2f9d04a65075a95dc0f40c0
rioxraman <ramanpkgupta@gmail.com>
2023-02-03 11:46:31 +0530

    nultiple controllers

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const router =require('./routes') 
-// app.use('/', )
+app.use('/',router )
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
-console.log("Router loaded");
+
+const homeController = require('../controllers/homeController');
+const userController = require('../controllers/userController');
+
+// console.log("Router loaded");
+router.get('/',homeController.home)
+router.get('/users',userController.profile)
 module.exports = router;
